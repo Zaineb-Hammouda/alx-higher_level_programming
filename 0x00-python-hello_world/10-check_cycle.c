5dce965aa0f8ca2fc2c3724244137fc0399991f0
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-06-06 04:01:55 +0200

    technical interview - check if singly linked list has a cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: pointer to head of the linked list
+ * Return: 0 if no cycle, 1 if cycle
+ */
+#define MAX_SIZE 150
+int check_cycle(listint_t *list)
+{
+	int i = 0;
+	listint_t *tmp;
+	listint_t *head;
+	listint_t **arr = malloc(sizeof(listint_t *) * MAX_SIZE);
+
+	head = list;
+	tmp = head;
+	tmp = tmp->next;
+	arr[0] = head;
+	arr[1] = NULL;
+	while (tmp)
+	{
+		for (i = 0; arr[i] != NULL; i++)
+		{
+			if (arr[i] == tmp)
+			{
+				free(arr);
+				return (1);
+			}
+		}
+		arr[i] = tmp;
+		arr[i + 1] = NULL;
+		tmp = tmp->next;
+	}
+	free(arr);
+	return (0);
+}
