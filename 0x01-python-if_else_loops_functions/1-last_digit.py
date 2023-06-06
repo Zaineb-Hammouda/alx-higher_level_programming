18f4f9f42c45e5833952dee1f2828c1590fd7820
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-06-07 00:12:09 +0200

    task 2- prints the ASCII alphabet, in lowercase, not followed by a new line

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,8 +4,7 @@ number = random.randint(-10000, 10000)
 if number >= 0:
     digit = number % 10
 else:
-    digit = abs(number) % 10
-    digit = -digit
+    digit = -(abs(number) % 10)
 if digit > 5:
     print("Last digit of", number, "is", digit, "and is greater than 5")
 elif digit == 0:
