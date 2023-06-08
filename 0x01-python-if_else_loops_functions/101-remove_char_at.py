92dd36898b649baa074bb630ba60ea1438506be8
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-06-09 01:14:49 +0200

    fixed task 101- return instead of print x(

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,7 +2,7 @@
 def remove_char_at(str, n):
     str1 = "" + str
     if n > len(str) or n < 0:
-        print(str1)
+        return(str1)
     else:
-        print(str1.replace(str1[n], ''))
+        return(str1.replace(str1[n], ''))
 
