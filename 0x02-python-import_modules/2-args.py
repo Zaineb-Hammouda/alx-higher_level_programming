70c661f8066852e78b206a9b0802381e533e7468
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-06-08 19:08:12 +0200

    task 2- make a script dynamic with command line args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    length = len(sys.argv) - 1
+    if length == 0:
+        print("0 arguemnts.")
+    elif length == 1:
+        print("1 argument:")
+        print("1:", sys.argv[1])
+    else:
+        print(f"{length} arguments:")
+        for i in range(1, length + 1):
+            print("{}: {}".format(i, sys.argv[i]))
