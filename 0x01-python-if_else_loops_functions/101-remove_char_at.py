cfd158128521dd3df36f27a82b08396cfd8268ee
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-06-08 03:19:27 +0200

    task 13- inserts a number into a sorted singly linked list in C

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     str1 = "" + str
-    if n > len(str1) or n < 0:
+    if n > len(str) or n < 0:
         print(str1)
     else:
         print(str1.replace(str1[n], ''))
+
---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_node - inserts a number into a sorted singly linked list
+ * Return: the address of a new node or NULL if failed
+ * @head: pointer to head pointer
+ * @number: data to insert
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *newNode = malloc(sizeof(listint_t));
+	listint_t *traverse = *head;
+
+	if (newNode == NULL)
+		return (NULL);
+	newNode->n = number;
+	newNode->next = NULL;
+	while (traverse != NULL)
+	{
+		if (traverse->n < number && traverse->next->n > number)
+		{
+			newNode->next = traverse->next;
+			traverse->next = newNode;
+		}
+		traverse = traverse->next;
+	}
+	return (newNode);
+}
