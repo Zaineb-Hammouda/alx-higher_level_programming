d8f5a7bae7e813379651eb29a1d28295d5cfd371
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-06-15 03:17:10 +0200

    returns a set of common elements in two sets

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    my_list = []
+    for i in set_1:
+        for j in set_2:
+            if j == i:
+                my_list.append(j)
+                my_set = set(my_list)
+    return my_set
