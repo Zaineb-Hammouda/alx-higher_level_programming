05a50b7948a4d4b3d9413369a9b7e71d1f524116
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-06-15 05:00:01 +0200

    task 9- eturns a new dictionary with all values multiplied by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     new_dict = a_dictionary.copy()
     for key and value in new_dict.items():
-        new_dict[k] = value * 2
+        new_dict[key] = value * 2
     return new_dict
