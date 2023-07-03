7323be07578af294f170329dd412f1ef53c2cdaa
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-07-04 00:29:23 +0200

    fixed pycodestyle

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ class square  that defines a square"""
+
+
 class Square:
     """ class that defines a square by
     private instance attribute: size
     instantiation with size """
-
     def __init__(self, size):
-
         """creates instance of square with argument size"""
         self.__size = size
