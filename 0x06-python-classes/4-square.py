3d69e0ba69bd20538ff33b52fcfcdbebe94521a3
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-07-04 01:08:22 +0200

    based on 3-square.py, with getter and setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" class square  that defines a square"""
+
+
+class Square:
+    """ class that defines a square by:
+    private instance attribute:
+        size
+    """
+
+    def __init__(self, size=0):
+        """creates instance of square with argument size"""
+        self.__size = size
+
+    def area(self):
+        """ returns area of the square """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """ retrieves size """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """raises errors
+        property setter for size
+        """
+        self.__size = value
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
