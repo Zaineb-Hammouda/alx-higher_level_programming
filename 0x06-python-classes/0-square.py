2157dab18e9b980fde5955f972521b75db516ad2
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-07-04 00:25:09 +0200

    task 1- class square that defines a square by private instance attribute: size

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ class square is an empty class that defines a square"""
+
 class Square:
     """ class that defines a square."""
+
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,6 +4,8 @@ class Square:
     """ class that defines a square by
     private instance attribute: size
     instantiation with size """
+
     def __init__(self, size):
+
         """creates instance of square with argument size"""
         self.__size = size
