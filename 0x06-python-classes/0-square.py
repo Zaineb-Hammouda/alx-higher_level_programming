eb5c09c0c93d423d335caa333db82207c1259507
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-07-03 23:53:45 +0200

    added discription for the class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """ class square is an empty class that defines a square"""
-
 class Square:
-    """ class that defines a square.
-    """
+    """ class that defines a square."""
     pass
