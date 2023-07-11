359ffeca0804346b330ac094a1a0525d5a25b49e
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-07-11 03:58:37 +0200

    task 1- fixed pycodestyle error

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -17,16 +17,20 @@ def matrix_divided(matrix, div):
     size = None
     for i in matrix:
         if not isinstance(i, list):
-            raise TypeError(
-                    'matrix must be a matrix (list of lists) of integers/floats')
+            raise TypeError("""
+                    matrix must be a matrix (
+                    list of lists) of integers/floats
+                    """)
         if size is None:
             size = len(i)
         elif size != len(i):
             raise TypeError("Each row of the matrix must have the same size")
         for j in i:
             if type(j) != int and type(j) != float:
-                raise TypeError(
-                        'matrix must be a matrix (list of lists) of integers/floats')
+                raise TypeError("""
+                        matrix must be a matrix (list of lists)
+                        of integers/floats
+                        """)
     if type(div) != int and type(div) != float:
         raise TypeError("div must be a number")
     if div == 0:
