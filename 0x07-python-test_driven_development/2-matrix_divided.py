e70634cd07fc49996e180020f98366b3f1944b73
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-07-11 04:01:29 +0200

    stored error msg in a variable to reduce line length

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -11,26 +11,20 @@ def matrix_divided(matrix, div):
     matrix: the matrix to divide
     div: the number to divide it by
     """
+    message = "matrix must be a matrix (list of lists) of integers/floats"
     if not isinstance(matrix, list):
-        raise TypeError(
-                'matrix must be a matrix (list of lists) of integers/floats')
+        raise TypeError(message)
     size = None
     for i in matrix:
         if not isinstance(i, list):
-            raise TypeError("""
-                    matrix must be a matrix (
-                    list of lists) of integers/floats
-                    """)
+            raise TypeError(message)
         if size is None:
             size = len(i)
         elif size != len(i):
             raise TypeError("Each row of the matrix must have the same size")
         for j in i:
             if type(j) != int and type(j) != float:
-                raise TypeError("""
-                        matrix must be a matrix (list of lists)
-                        of integers/floats
-                        """)
+                raise TypeError(message)
     if type(div) != int and type(div) != float:
         raise TypeError("div must be a number")
     if div == 0:
