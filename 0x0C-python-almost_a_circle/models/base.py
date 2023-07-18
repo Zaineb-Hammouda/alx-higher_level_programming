a162bf5120700c75683b7a0442d060883ac107d9
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-07-18 14:00:17 +0200

    task 15- add static method to_json_string to class Base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,7 @@
 the base module
 defines a class Base
 """
+import json
 
 
 class Base:
@@ -20,3 +21,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string rep of list_dictionnaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
