aca05167e8985c4e2adc812487d20ccb2abfaa9e
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2024-02-13 00:46:18 +0100

    task 2- command line arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,13 +3,11 @@
 const num = process.argv.length;
 
 if (num <= 2) {
-  console.log('No argument');
-
+	console.log('No argument');
 } else {
-  if (num === 3) {
-  console.log('Argument found');
-
+	if (num === 3) {
+		console.log('Argument found');
 } else {
-  console.log('Arguments found');
+	console.log('Arguments found');
 }
 }
