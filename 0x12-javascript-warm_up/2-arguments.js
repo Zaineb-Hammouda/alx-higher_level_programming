5cd4e4739e0572a1abf89165e4a5da61ae73b5e8
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2024-02-13 01:15:16 +0100

    fixing

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,10 +4,13 @@ const num = process.argv.length;
 
 if (num <= 2) {
 	console.log('No argument');
+
 } else {
 	if (num === 3) {
 		console.log('Argument found');
+
 } else {
 	console.log('Arguments found');
+
 }
 }
