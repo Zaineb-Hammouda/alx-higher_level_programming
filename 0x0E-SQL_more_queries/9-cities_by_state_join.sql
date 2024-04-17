82335bb57ad01e8a8d7f613f7e1dd18896f81b10
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2024-04-16 17:48:12 -0700

    task 9- list all cities in database

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 --  a script that lists all the cities contained in the database hbtn_0d_usa
 SELECT c.id, c.name, s.name
 FROM cities AS c
-FULL JOIN states AS s
+LEFT JOIN states AS s
 ON s.id= c.state_id
 ORDER BY c.id ASC;
